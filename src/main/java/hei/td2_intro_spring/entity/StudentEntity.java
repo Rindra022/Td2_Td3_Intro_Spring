a89d936144faa6c3d5c6329bd8854f515d7334f3
Rindra <hei.rindra.2@gmail.com>
2026-03-19 23:22:00 +0300

    initialized student entity

---
+++ src/main/java/hei/td2_intro_spring/entity/StudentEntity.java
@@ -0,0 +1,42 @@
+package hei.td2_intro_spring.entity;
+
+public class StudentEntity {
+    private String reference;
+    private String firstName;
+    private String lastName;
+    private Integer age;
+
+    public StudentEntity(){}
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
