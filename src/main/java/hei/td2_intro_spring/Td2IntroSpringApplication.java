f36c25d8dc0c2c07d2b8b19851df2203a83b3d0f
Rindra <hei.rindra.2@gmail.com>
2026-03-19 21:32:40 +0300

    Initial commit

---
+++ src/main/java/hei/td2_intro_spring/Td2IntroSpringApplication.java
@@ -0,0 +1,13 @@
+package hei.td2_intro_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Td2IntroSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Td2IntroSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hei/td2_intro_spring/Td2IntroSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.td2_intro_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Td2IntroSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
