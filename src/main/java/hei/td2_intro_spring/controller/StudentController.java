d83c49815affc3c7cf3a12a4678795645336a1e7
Rindra <hei.rindra.2@gmail.com>
2026-03-20 00:16:40 +0300

    add methode get all students with condition accept

--- src/main/java/hei/td2_intro_spring/controller/StudentController.java
+++ src/main/java/hei/td2_intro_spring/controller/StudentController.java
@@ -2,6 +2,8 @@ package hei.td2_intro_spring.controller;
 
 import hei.td2_intro_spring.entity.StudentEntity;
 import hei.td2_intro_spring.service.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,20 @@ public class StudentController {
 
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllStudents(@RequestHeader("Accept") String accept) {
+        if(accept.equals("text/plain")){
+            String allNames = String.join("\n", studentService.getAllStudentsFullName());
+            return ResponseEntity.ok()
+                    .contentType(MediaType.TEXT_PLAIN)
+                    .body(allNames);
+        }else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_ACCEPTABLE)
+                    .body("Format not supported ");
+        }
     }
 
 
