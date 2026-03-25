a3fb2dc77a8b77c7d650772b7f938c34fe6cd239
Rindra <hei.rindra.2@gmail.com>
2026-03-25 21:39:01 +0300

    feat: add StudentValidator and support text/plain output in StudentService

--- src/main/java/hei/td2_intro_spring/service/StudentService.java
+++ src/main/java/hei/td2_intro_spring/service/StudentService.java
@@ -2,17 +2,21 @@ package hei.td2_intro_spring.service;
 
 import hei.td2_intro_spring.entity.StudentEntity;
 import hei.td2_intro_spring.repository.StudentRepository;
+import hei.td2_intro_spring.validator.StudentValidator;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
 public class StudentService {
     private final StudentRepository studentRepository;
+    private final StudentValidator studentValidator;
 
-    public StudentService(StudentRepository studentRepository) {
+    public StudentService(StudentRepository studentRepository, StudentValidator studentValidator) {
         this.studentRepository = studentRepository;
+        this.studentValidator = studentValidator;
     }
 
     public List<String> getAllStudentsFullName() {
@@ -25,10 +29,20 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
-    public void createStudents(List<StudentEntity> students) {
-        studentRepository.save(students);
+    public String getAllStudentsAsText(){
+        return studentRepository.findAll().stream()
+                .map(s -> String.join(" ",
+                        s.getReference(),
+                        s.getFirstName(),
+                        s.getLastName()
+                ))
+                .collect(Collectors.joining("\n"));
+
     }
 
-
+    public void createStudents(List<StudentEntity> students) {
+        studentValidator.validate(students);
+        studentRepository.save(students);
+    }
 
 }
